9c1adba3287928ebfb4c9c3b445552377dbe3dbd
Jason B. Blazzard <blazzard@gmail.com>
2016-09-22 11:36:33 -0600

    working commit for straight web

---
+++ popup.js
@@ -0,0 +1,17 @@
+﻿
+//    document.addEventListener('DOMContentLoaded', function () {
+//        var hText = '';
+//        var wCount  = 0;
+//        hText = (document.all) ? document.selection.createRange().text : document.getSelection();
+//        hText = hText.toString();
+//        wCount = hText.split(" ").length;
+//        document.getElementById('wordCount').innerHTML = wCount;  
+//    }, false);
+
+    chrome.tabs.executeScript({
+        code: "window.getSelection().toString();"
+    }, function (selection) {
+        var hText = selection.toString();
+        var wCount = hText.split(" ").length;
+        document.getElementById('wordCount').innerHTML = wCount;
+    });
