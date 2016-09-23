5807e82812dcb95c9847faa801ce6174fe465a42
Jason Blazzard <blazzard@gmail.com>
2016-09-23 14:49:38 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,17 +1,7 @@
-﻿
-//    document.addEventListener('DOMContentLoaded', function () {
-//        var hText = '';
-//        var wCount  = 0;
-//        hText = (document.all) ? document.selection.createRange().text : document.getSelection();
-//        hText = hText.toString();
-//        wCount = hText.split(" ").length;
-//        document.getElementById('wordCount').innerHTML = wCount;  
-//    }, false);
-
-    chrome.tabs.executeScript({
-        code: "window.getSelection().toString();"
-    }, function (selection) {
-        var hText = selection.toString();
-        var wCount = hText.split(" ").length;
-        document.getElementById('wordCount').innerHTML = wCount;
-    });
+chrome.tabs.executeScript({
+    code: "window.frame[0].getSelection().toString();"
+}, function (selection) {
+    var hText = selection.toString();
+    var wCount = hText.split(" ").length;
+    document.getElementById('wordCount').innerHTML = wCount;
+});
