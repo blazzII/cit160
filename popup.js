dde7fa7986984b6a8d806c0d7e1042f9fc4725b0
Blazzard <blazzard@gmail.com>
2022-01-06 08:58:15 -0700

    Clean up

--- popup.js
+++
@@ -1,7 +0,0 @@
-chrome.tabs.executeScript({
-    code: "window.frame[0].getSelection().toString();"
-}, function (selection) {
-    var hText = selection.toString();
-    var wCount = hText.split(" ").length;
-    document.getElementById('wordCount').innerHTML = wCount;
-});
